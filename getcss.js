ad0d69e980cc5e775ecc12623ab948541a2823f3
John Otander <johnotander@gmail.com>
2015-10-05 10:15:51 -0600

    Use a json array for urls

--- getcss.js
+++ getcss.js
@@ -6,113 +6,18 @@ var _ = require('lodash')
 var filesize = require('filesize')
 var cssstats = require('cssstats')
 var getcss = require('get-css')
-
+var urls = require('./urls.json')
 
 var options = {
   timeout: 5000
-};
+}
 
-var github = getcss('http://github.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/github.css', response.css)
+urls.forEach(function (url) {
+  getcss(url, options).then(function (response) {
+    var domain = url.replace(/.*?:\/\//g, '')
+    fs.writeFileSync('./data/' + domain + '.css', response.css)
   })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var pinterest = getcss('https://pinterest.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/pinterest.css', response.css)
+  .catch(function (error) {
+    console.log("Can't get css because" + error)
   })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var pinterest = getcss('http://mapbox.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/mapbox.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var bluebottlecoffee = getcss('http://bluebottlecoffee.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/bluebottlecoffee.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-
-var instagram = getcss('http://instagram.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/instagram.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var etsy = getcss('http://etsy.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/etsy.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var apple = getcss('http://apple.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/apple.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var kickstarter = getcss('http://kickstarter.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/kickstarter.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var twitter = getcss('http://twitter.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/twitter.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var tumblr = getcss('http://tumblr.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/tumblr.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var soundcloud = getcss('http://soundcloud.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/soundcloud.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var stripe = getcss('http://stripe.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/stripe.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
-
-var bbc = getcss('http://bbc.com', options)
-  .then(function(response) {
-    fs.writeFileSync('./data/bbc.css', response.css)
-  })
-  .catch(function(error) {
-     console.log("Can't get css because" + error)
-  });
+})
